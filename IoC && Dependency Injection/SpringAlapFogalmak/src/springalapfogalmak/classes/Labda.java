f090cab16288f0bb16baecbc578ca97e0c058bc2
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2020-07-19 17:42:40 +0200

    Add files via upload
    forras

---
+++ IoC && Dependency Injection/SpringAlapFogalmak/src/springalapfogalmak/classes/Labda.java
@@ -0,0 +1,13 @@
+
+package springalapfogalmak.classes;
+
+public class Labda {
+
+    public Labda() {
+        System.out.println("Labda");
+    }
+    
+    public void jatek(){
+        System.out.println("Játék");
+    }
+}
