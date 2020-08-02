beb97fa6f5fbe32ad736a62326a7ba8e7a162cdd
FiglerReNi <renatafigler85@gmail.com>
2020-08-02 16:46:53 +0200

    forras

---
+++ SpringElso/src/main/java/com/springelso/SpringElsoApplication.java
@@ -0,0 +1,13 @@
+package com.springelso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElsoApplication.class, args);
+	}
+
+}
---
+++ SpringElso/src/test/java/com/springelso/SpringElsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springelso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
