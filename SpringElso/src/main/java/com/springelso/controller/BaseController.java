403488944b761b74dd2911a23cdbf789a8e977cd
FiglerReNi <renatafigler85@gmail.com>
2020-08-09 12:27:26 +0200

    forras

---
+++ SpringElso/src/main/java/com/springelso/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.springelso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+	/*Ilyen esetben egy nézetet keresne oldalneve néven, amit megjeleníthet*/
+	/*@RequestMapping("/")
+	public String index() {
+		return "oldalneve";
+	}*/
+}
---
+++ SpringElso/src/main/java/com/springelso/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springelso.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	/*ha a static-ban van index.html fájl is a RestController nyer, ez kerül megjelenítésre*/
+	/*@RequestMapping("/")
+	public String index() {
+		return "Teszt";
+	}*/
+
+}
