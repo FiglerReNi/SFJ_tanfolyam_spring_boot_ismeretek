092cf3236fcd73fc48fdd56caa93b2a287d4be43
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2021-06-26 20:15:55 +0200

    forras

--- SpringProject/src/main/java/com/springproject/service/SpyGirlService.java
+++ SpringProject/src/main/java/com/springproject/service/SpyGirlService.java
@@ -3,7 +3,6 @@ package com.springproject.service;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-//Ezzel az annotációval jelezzük hogy ő egy bean, az életciklusa pedig session.
 @Component("ujNev")
 @Scope(value="prototype")
 public class SpyGirlService {
